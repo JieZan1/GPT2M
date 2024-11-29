cb2ee586d413f5cd793d712174b80c80e2626a18
Jie Zan <jie.zan@mail.mcgill.ca>
2024-11-28 22:11:27 -0500

    Create setup_conda.sh

---
+++ setup_conda.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=01:00:00
+#SBATCH --output=%N-%j.out
+#SBATCH --account=fall2024-comp551
+
+module load miniconda/miniconda-fall2024
+conda init bash
+
+
+
+
+
