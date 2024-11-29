7c65c449153e34e8fee2c8876b78e16c3ed352d2
Jie Zan <jie.zan@mail.mcgill.ca>
2024-11-28 21:48:33 -0500

    test

---
+++ launch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --node 1
+#SBATCH --gres=gpu:1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=01:00:00
+#SBATCH --output=%N-%j.out
+#SBATCH --account=fall2024-comp551
+
+module load cuda/cuda-12.6
+module load miniconda/miniconda-fall2024
+source ~/.bashrc
+conda activate test_jf
+
+echo "Test" > test_out.txt
+nvidia_smi >> test_out.txt
+python xxx.py
+
+
+
+
+
