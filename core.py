9428b56943875aa6077e2817a326d1c1f3f82a86
Jie Zan <jie.zan@mail.mcgill.ca>
2024-12-02 20:02:20 -0500

    ok

--- core.py
+++ core.py
@@ -51,3 +51,34 @@ train_dataset = Dataset(X_train_tokenized, train_one_label["labels"])
 val_dataset = Dataset(X_val_tokenized, validation_one_label["labels"])
 
 print(X_train_tokenized.keys())
+
+def compute_metrics(p):
+    print(type(p))
+    pred, labels = p
+    pred = np.argmax(pred, axis=1)
+
+    accuracy = accuracy_score(y_true=labels, y_pred=pred)
+    recall = recall_score(y_true=labels, y_pred=pred)
+    precision = precision_score(y_true=labels, y_pred=pred)
+    f1 = f1_score(y_true=labels, y_pred=pred)
+
+    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
+
+# Define Trainer
+args = TrainingArguments(
+    output_dir="output",
+    num_train_epochs=1,
+    per_device_train_batch_size=8
+
+)
+trainer = Trainer(
+    model=model,
+    args=args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    compute_metrics=compute_metrics
+)
+
+trainer.train()
+
+print(trainer.evaluate())
